c59d98457c4457889601254e7f3cc059c061bded
Anushka786 <58993716+Anushka786@users.noreply.github.com>
2019-12-17 11:49:50 -0800

    Update dummy_newsfeedData.dart

--- lib/dummy_newsfeedData.dart
+++ lib/dummy_newsfeedData.dart
@@ -9,7 +9,26 @@ List<Notice> noticeItems=[
   Notice(
     id:'n2',
     title: 'Important Meeting',
-    content: 'Meeting tomorrow at 10pm in the meeting hall for discussing about the expectations of society members.',
+    content: 'Meeting tomorrow at 6pm in the meeting hall for discussing about the expectations of society members.',
     isFavorite: false,
   ),
+  Notice(
+    id:'n3',
+    title: 'Important Meeting',
+    content: 'Meeting tomorrow at 10pm in the meeting hall for discussing about new year celebration.',
+    isFavorite: false,
+  ),
+  Notice(
+    id:'n4',
+    title: 'New Year\'s Celebtration! ',
+    content: 'Mark your calenders for the celebration of New Year\'s at society\'s Club house on Sunday at 8pm.',
+    isFavorite: false,
+  ),
+  Notice(
+    id:'n5',
+    title: 'Important Meeting',
+    content: 'Meeting tomorrow at 9pm in the meeting hall for discussing new policies in society.',
+    isFavorite: false,
+  ),
+  
 ];
